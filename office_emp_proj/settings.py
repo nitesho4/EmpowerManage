b47dc2c7f063bc06ef72b5e28f9b76e5ca55d22b
Nitesh Choudhary <115777259+nitesho4@users.noreply.github.com>
2024-06-24 16:58:53 +0530

    Update settings.py

--- office_emp_proj/settings.py
+++ office_emp_proj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-04n$_ed7imuc#a1)407lut0-rs%@i6#a(dbo--ihp2r4_lro$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
